3169b614c79a44ae825ef36ffb14e08492428de0
Roha-Lee <roha.lee@buzzvil.com>
2022-09-13 05:21:41 +0900

    learn function

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("roha")
+	fmt.Println(totalLength, upperName)
 
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
