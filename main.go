502df574d586643223e34c82f42cc9716f701242
Roha-Lee <roha.lee@buzzvil.com>
2022-09-13 09:36:12 +0900

    study switch

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	repeatMe("Hello", "My", "Name", "is", "Roha")
 	totalLength, upperName = nakedReturn("gru")
 	fmt.Println(totalLength, upperName)
+	superAdd(1, 3, 2, 4, 5, 6, 1)
+	fmt.Println(canIDrink(31), canIDrink(17))
 
 }
 
@@ -33,4 +35,25 @@ func nakedReturn(name string) (length int, uppercase string) {
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
+}
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for i := 0; i < len(numbers); i++ {
+		total += numbers[i]
+	}
+	for index, number := range numbers {
+		fmt.Println(index, number)
+	}
+	return total
+}
+
+func canIDrink(age int) bool {
+	switch koreanAge := age + 2; {
+	case koreanAge > 19:
+		return true
+	case koreanAge <= 19:
+		return false
+	}
+	return false
 }
