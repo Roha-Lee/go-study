b3ee0e75800c95dd3491545443a3f2c36559f00f
Roha-Lee <roha.lee@buzzvil.com>
2022-09-14 22:21:18 +0900

    Create Account Struct

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"std/github.com/roha-lee/go-study/banking"
+)
+
+func main() {
+	account := banking.Account{Owner: "Roha Lee", Balance: 10000000}
+	fmt.Println(account)
+}
